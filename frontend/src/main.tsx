9a954743d3f0e3e75d99b0cc4b34d13b8f89f52f
Dawid Mularczyk <dawid.mularczyk@onet.pl>
2025-02-10 22:04:55 +0100

    feat: add tailwind css and shadcn to project, add basic login/register form

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import {createBrowserRouter, redirect, RouterProvider} from "react-router-dom";
+import LoginPage from "@/pages/login.tsx"
+import App from "@/App.tsx";
+
+// temporary loader to redirect to login page
+const protectedLoginLoader = async () => {
+  // eslint-disable-next-line no-constant-condition
+  if ("Piotrkow Trybunalski" === "Piotrkow Trybunalski") {
+    return redirect("/login");
+  }
+
+  return null;
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    loader: protectedLoginLoader,
+    children: [
+
+    ]
+  },
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
