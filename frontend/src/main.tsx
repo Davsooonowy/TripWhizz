84c2420769a2bf934f357681192497d03bf3fa6c
Dawid Mularczyk <72767335+Davsooonowy@users.noreply.github.com>
2025-02-12 00:57:35 +0100

    Update frontend/src/main.tsx
    Co-authored-by: Mateusz Maciaszczyk <146715889+MateuszMaciaszczyk@users.noreply.github.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import {createBrowserRouter, redirect, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, redirect, RouterProvider } from "react-router-dom";
 import LoginPage from "@/pages/login.tsx"
 import App from "@/App.tsx";
 
