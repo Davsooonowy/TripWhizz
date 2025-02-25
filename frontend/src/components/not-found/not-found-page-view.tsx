2130bda6f8558fc784f3090c0481fd81cd82dd31
Dawid Mularczyk <dawid.mularczyk@onet.pl>
2025-02-25 23:44:43 +0100

    fix: fix mobile error page
    ( oopsie )

--- frontend/src/components/not-found/not-found-page-view.tsx
+++ frontend/src/components/not-found/not-found-page-view.tsx
@@ -4,8 +4,11 @@ import sealTravelGif from '@/assets/seal-travel.gif';
 
 export function NotFoundView() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="max-w-md space-y-4">
+    <div className="flex flex-col items-center justify-center min-h-screen md:flex-row">
+      <div className="w-72 h-72 rounded-full flex items-center justify-center mb-4 md:mb-0 md:ml-10 border border-background">
+        <img src={sealTravelGif} alt="Traveling Seal" className="w-full h-full object-cover rounded-full" />
+      </div>
+      <div className="max-w-md space-y-4 text-center md:text-left">
         <h1 className="text-6xl font-extrabold text-gray-900">Off Course!</h1>
         <p className="text-lg text-gray-700">
           Your plane is circling the Earth, but it’s drifted off course
@@ -13,10 +16,6 @@ export function NotFoundView() {
         <Link to="/" className="inline-flex items-center gap-2 text-primary hover:text-accent transition">
           <ArrowLeft size={16} /> Bring me back Home
         </Link>
-      </div>
-
-      <div className="w-72 h-72 rounded-full flex items-center justify-center ml-10 border border-background">
-        <img src={sealTravelGif} alt="Traveling Seal" className="w-full h-full object-cover rounded-full" />
       </div>
     </div>
   );
