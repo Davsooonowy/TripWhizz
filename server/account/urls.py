f250fac04b1fc5bf8ea397ad8acd3f4da8c3d7c6
MateuszMaciaszczyk <maciaszczyk@student.agh.edu.pl>
2025-03-29 15:52:00 +0100

    fix: deleted unnecessary serializers and views

--- server/account/urls.py
+++ server/account/urls.py
@@ -1,20 +1,20 @@
 from django.urls import path
 
-from .views import AddUserView, LoginView, PasswordResetView, UserView
+from . import views
 
 urlpatterns = [
-    path("user/", AddUserView.as_view(), name="user_operations"),
-    path("user/<int:user_id>/", UserView.as_view(), name="user_delete"),
-    path("user/login/", LoginView.as_view(), name="user_login"),
-    path("user/me/", UserView.as_view(), name="current_user"),
+    path("user/", views.AddUserView.as_view(), name="user_operations"),
+    path("user/<int:user_id>/", views.UserView.as_view(), name="user_delete"),
+    path("user/login/", views.LoginView.as_view(), name="user_login"),
+    path("user/me/", views.UserView.as_view(), name="current_user"),
     path(
         "user/password-reset/",
-        PasswordResetView.as_view(),
+        views.PasswordResetView.as_view(),
         name="password_reset_request",
     ),
     path(
         "user/password-reset-confirm/<uidb64>/<token>/",
-        PasswordResetView.as_view(),
+        views.PasswordResetView.as_view(),
         name="password_reset_confirm",
     ),
 ]
