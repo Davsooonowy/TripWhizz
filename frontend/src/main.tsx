95c2b2b0892aec8f1d29ea2597438a0cb52e10e8
MateuszMaciaszczyk <maciaszczyk@student.agh.edu.pl>
2025-04-28 13:35:57 +0200

    feat: frontend for stages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -44,6 +44,7 @@ const PrivateTripInvitePage = React.lazy(
 const PublicTripInvitePage = React.lazy(
   () => import('@/pages/trip/new/public/invite'),
 );
+const StageDetailsPage = React.lazy(() => import('@/pages/trip/new/private/stages/stage-details'));
 
 const protectedLoginLoader = async () => {
   if (!authenticationProviderInstance.isAuthenticated()) {
@@ -76,6 +77,10 @@ const router = createBrowserRouter([
       {
         path: '/trip',
         element: <StartTripPage />,
+      },
+      {
+        path: '/trip/:tripId/stages/:stageId',
+        element: <StageDetailsPage />,
       },
       {
         path: '/trip/new',
