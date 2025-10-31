caf85faed85ea4da15f5cb19b1e82d1f6be7909f
Dawid Mularczyk <dawid.mularczyk@onet.pl>
2025-10-31 02:06:53 +0100

    fix: fix tripId to chagne after map change

--- frontend/src/pages/trip/[id]/itinerary/maps.tsx
+++ frontend/src/pages/trip/[id]/itinerary/maps.tsx
@@ -1,8 +1,19 @@
 import TripMaps from '@/components/itinerary/maps';
+import { useTripContext } from '@/components/util/trip-context';
 
-import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function TripMapsPage() {
   const { tripId } = useParams();
+  const { selectedTrip } = useTripContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (selectedTrip?.id && tripId && selectedTrip.id.toString() !== tripId) {
+      navigate(`/trip/${selectedTrip.id}/maps`, { replace: true });
+    }
+  }, [selectedTrip?.id, tripId, navigate]);
+
   return <TripMaps tripId={tripId} />;
 }
